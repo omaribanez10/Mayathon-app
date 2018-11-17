a2b6c0e56594233918617d661acb38c2c54e390a
unknown <omaribanez100@gmail.com>
2018-11-17 04:44:58 -0500

    segundo commit agregando detalle solicitud

--- app/src/main/java/unab/dev/appmayasoft/recyclerSolicitudes.java
+++ app/src/main/java/unab/dev/appmayasoft/recyclerSolicitudes.java
@@ -2,16 +2,20 @@ package unab.dev.appmayasoft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 
 public class recyclerSolicitudes extends AppCompatActivity {
 
-    RecyclerView recyclerviewServicio;
+    RecyclerView recyclerviewSolicitud;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_solicitudes);
 
-        recyclerviewServicio = findViewById(R.id.recyclerviewServicio);
+        recyclerviewSolicitud = findViewById(R.id.recyclerviewSolicitud);
+
+
     }
 }
