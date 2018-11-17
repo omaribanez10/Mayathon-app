25b95e426d0ff3bd05ef885bfdbe3efd660d39f1
unknown <omaribanez100@gmail.com>
2018-11-17 12:34:10 -0500

    commit arreglando recycler solicitudes

---
+++ app/src/main/java/unab/dev/appmayasoft/Startups_Invertir.java
@@ -0,0 +1,13 @@
+package unab.dev.appmayasoft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Startups_Invertir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startups__invertir);
+    }
+}
